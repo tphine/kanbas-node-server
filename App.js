41c451b4ba05a00e78437311a5a534d176f2d7ed
tphine <tristanphan7@gmail.com>
2024-03-31 22:22:34 -0400

    first commit

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import Lab5 from './Lab5.js';
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
+import cors from 'cors';
 import Hello from "./Hello.js"
 const app = express();
+app.use(cors());
+app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
 Lab5(app);
 Hello(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
