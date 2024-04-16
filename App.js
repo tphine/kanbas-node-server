dfc6dbe4918a0f52d5bfba205064798f080a7d75
tphine <tristanphan7@gmail.com>
2024-04-16 03:14:58 -0400

    reorder arguments

--- App.js
+++ App.js
@@ -11,10 +11,6 @@ import Hello from "./Hello.js"
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
 mongoose.connect(CONNECTION_STRING);
 const app = express();
-app.use(cors({
-    credentials: true,
-    origin: process.env.FRONTEND_URL
-}));
 const sessionOptions = {
     secret: process.env.SESSION_SECRET,
     resave: false,
@@ -29,6 +25,10 @@ if (process.env.NODE_ENV !== "development") {
         domain: process.env.HTTP_SERVER_DOMAIN,
     };
 }
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+}));
 app.use(express.json());
 UserRoutes(app);
 ModuleRoutes(app);
