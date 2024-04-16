959c13915c0f79f947cf8e0f090b723d87650787
tphine <tristanphan7@gmail.com>
2024-04-16 03:33:28 -0400

    edit session

--- App.js
+++ App.js
@@ -11,20 +11,19 @@ import Hello from "./Hello.js"
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
 mongoose.connect(CONNECTION_STRING);
 const app = express();
-const sessionOptions = {
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-};
-app.use(session(sessionOptions));
-if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-        sameSite: "none",
-        secure: true,
-        //domain: process.env.HTTP_SERVER_DOMAIN,
-    };
-}
+app.use(
+    session({
+        secret: "keyboard cat",
+        resave: false,
+        saveUninitialized: false,
+        proxy: true,
+        cookie: {
+            sameSite: "none",
+            secure: true,
+            domain: "kanbas-node-server-aid2.onrender.com",
+        },
+    })
+);
 app.use(cors({
     credentials: true,
     origin: process.env.FRONTEND_URL
